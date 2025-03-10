d6246f95ab486180a6f6a58285ccb1b699285ba0
Ravichander0512 <ravichander0512@gmail.com>
2025-03-10 23:06:37 +0530

    App Creation

---
+++ src/main/java/com/india/agriculture/EAgricultureApplication.java
@@ -0,0 +1,13 @@
+package com.india.agriculture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAgricultureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAgricultureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/india/agriculture/EAgricultureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.india.agriculture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAgricultureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
